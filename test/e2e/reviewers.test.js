53bfd1bcd0f7b91e8b5de10d9361f5a6759abec4
mugworks <greenwood.pdx@gmail.com>
2017-10-26 09:48:32 -0700

    fix reviewers get all

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -94,10 +94,10 @@ describe('Reviewer API', () => {
             });
     });
 
-    it('get all reviewers',() => {
+    it.only('get all reviewers',() => {
 
         let reviewer1 = {
-            name: 'Roger Ebert',
+            name: 'Erdem Ebert',
             company: 'Siskel & Ebert'
         };
 
@@ -118,7 +118,7 @@ describe('Reviewer API', () => {
                 return request.get('/api/filmIndustry/reviewers');
             })
             .then(res => {
-                assert.deepEqual(res.body.name, saved.name);
+                assert.deepEqual(res.body[1].name, saved[0].name);
             });
     });
 
